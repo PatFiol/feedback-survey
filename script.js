8277487f684e20f260fbf9044d916885c0149578
Patricia Fiol <patricia.fiol@outlook.com>
2023-01-30 07:36:31 +0100

    add script

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-document.getElementById('question').innerHTML =
-	'What do you think of the quality of our products?'
 
-const ratings = document.querySelectorAll('rating')
-const showQuestion = document.querySelectorAll('questions')
-const nextBtn = document.querySelector('#next')
-// const sendBtn = document.querySelectorAll('#panel')
-
-let questionIndex = 0
-
-const questions = [
-	'How do you feel about your experience?',
-	'What do you think of the quality of our products?',
-	'How was the service of our staff?',
-	'What are the chances for you visiting us again?',
-]
-
-function nextQuestion() {
-	questionIndex++
-	console.log(questionIndex)
-	document.querySelector('#question').innerHTML = questions[questionIndex]
-}
-
-nextBtn.addEventListener('click', nextQuestion)
+const sendBtn = document.querySelector('#submit')
+const welcome = document.querySelector('.welcome')
 
 
+sendBtn.addEventListener('click', (e) => {
+	panel.remove();
+	sendBtn.remove();
+	welcome.innerHTML = `
+	<i class="fa-solid fa-heart"></i>
+	Thank you for your feedback!
+	`
+})
