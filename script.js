cd747f50c842c92d8b04f4a523ba0bd137189014
Patricia Fiol <patricia.fiol@outlook.com>
2023-07-26 18:49:54 +0200

    add questions and functionality

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 
-const sendBtn = document.querySelector('#submit')
-const welcome = document.querySelector('.welcome')
+const submitBtn = document.getElementById('submit')
+const message = document.querySelector('.header')
 
 
-sendBtn.addEventListener('click', (e) => {
+// Change question automatically
+function updateQuestion() {
+  const questions = [
+    'How do you feel about your experience?',
+    'What do you think of the quality of our products?',
+    'How was the service of our staff?',
+    'What are the chances for you visiting us again?'
+  ];
+
+  const questionElement = document.getElementById('question');
+  let currentQuestionIndex = parseInt(questionElement.dataset.questionIndex) || 0;
+
+  if (currentQuestionIndex === questions.length - 1) {
+    const ratingContainer = document.querySelector('.rating-container');
+    ratingContainer.removeEventListener('click', updateQuestion);
+    submitBtn.style.display = 'block';
+  } else {
+    currentQuestionIndex += 1;
+    questionElement.dataset.questionIndex = currentQuestionIndex;
+    questionElement.textContent = questions[currentQuestionIndex];
+  }
+}
+
+// Add event listener to the rating container
+const ratingContainer = document.querySelector('.rating-container');
+ratingContainer.addEventListener('click', updateQuestion);
+
+
+// Hide panel and display Thank you message
+submitBtn.addEventListener('click', (e) => {
 	panel.remove();
-	sendBtn.remove();
-	welcome.innerHTML = `
-	<i class="fa-solid fa-heart"></i>
+	submitBtn.remove();
+	message.innerHTML = `
+	<li><i class="fa-solid fa-heart"></i></li>
 	Thank you for your feedback!
 	`
 })
